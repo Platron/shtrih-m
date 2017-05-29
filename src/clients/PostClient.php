efa88ac72f42810291cc096151b32433bbd1e459
lashnev <a.lashnev@platron.ru>
2017-05-29 14:53:21 +0300

    Psr3 логирование

--- src/clients/PostClient.php
+++ src/clients/PostClient.php
@@ -4,20 +4,27 @@ namespace Platron\Shtrihm\clients;
 
 use Platron\Shtrihm\SdkException;
 use Platron\Shtrihm\services\BaseServiceRequest;
+use Psr\Log\LoggerInterface;
 
 class PostClient implements iClient {
+    
+    const LOG_LEVEL = 0;
     
     /** @var string Строка приватного ключа */
     protected $secretKey;
     /** @var int Последний код http ответа */
     protected $lastHttpCode;
+    /** @var LoggerInterface */
+    protected $logger;
     
     /**
      * Секретный ключ для подписи запросов
      * @param string $secretKey
+     * @param LoggerInterface $logger
      */
-    public function __construct($secretKey){
+    public function __construct($secretKey, LoggerInterface $logger = null){
         $this->secretKey = $secretKey;
+        $this->logger = $logger;
     }
     
     /**
@@ -25,8 +32,9 @@ class PostClient implements iClient {
      */
     public function sendRequest(BaseServiceRequest $service) {
         $requestParameters = $service->getParameters();
+        $requestUrl = $service->getRequestUrl();
         
-        $curl = curl_init($service->getRequestUrl());
+        $curl = curl_init($requestUrl);
 		curl_setopt($curl, CURLOPT_POST, 1);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($requestParameters));
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -34,6 +42,11 @@ class PostClient implements iClient {
         // Нужно сделать подпись документа перед отправкой
         
         $response = curl_exec($curl);
+        
+        if($this->logger){
+            $this->logger->log(self::LOG_LEVEL, 'Requested url '.$requestUrl.' params '. print_r($requestParameters, true));
+            $this->logger->log(self::LOG_LEVEL, 'Response '.$response);
+        }
 		
 		if(curl_errno($curl)){
 			throw new SdkException(curl_error($curl), curl_errno($curl));
